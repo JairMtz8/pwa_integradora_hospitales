6439cd9665405b4524219ac7a26ee8a6a918cea9
Jair Antonio Martínez Valladares <20223tn068@utez.edu.mx>
2025-12-09 14:37:15 -0600

    Status

--- env.js
+++ env.js
@@ -1,2 +1,2 @@
 // Cambia esta IP cuando estés en otra red (escuela, casa, etc.)
-const API_BASE_URL = "https://pwahotelesnavaez-production.up.railway.app/api";
+const API_BASE_URL = "192.168.100.184:8080/api";
--- js/sync-incidencias.js
+++ js/sync-incidencias.js
@@ -2,7 +2,7 @@
 import db from "./pouchdb-config.js";
 import { subirFotosAStorage } from "./subirFotosAStorage.js";
 
-const API_BASE_URL = "https://pwahotelesnavaez-production.up.railway.app/api";
+const API_BASE_URL = "https://192.168.100.184:8080/api";
 
 /**
  * Sincroniza todas las incidencias guardadas en PouchDB
